2052e441238b9b85eccddd051d21a469854be278
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-15 16:54:58 -0400

    Fixed redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
           <h1 className='w-1/2 mx-auto text-center mb-2'>M'KENNA & PATRICK</h1>
           <br />
           <div className='flex justify-evenly'>
-            <a className='hover:underline underline-offset-8' href=".">Home</a>
-            <a className='hover:underline underline-offset-8' href=".">Wedding Party</a>
-            <a className='hover:underline underline-offset-8' href=".">Schedule</a>
-            <a className='hover:underline underline-offset-8' href=".">Gallery</a>
-            <a className='hover:underline underline-offset-8' href=".">RSVP</a>
+            <a className='hover:underline underline-offset-8' href="/">Home</a>
+            <a className='hover:underline underline-offset-8' href="/">Wedding Party</a>
+            <a className='hover:underline underline-offset-8' href="/">Schedule</a>
+            <a className='hover:underline underline-offset-8' href="/">Gallery</a>
+            <a className='hover:underline underline-offset-8' href="/">RSVP</a>
           </div>
           </nav>
         {children}
