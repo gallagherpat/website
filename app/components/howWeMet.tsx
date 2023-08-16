b06a8d20805ee1124d7af76abec6c7f49b3a2b14
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-15 22:36:12 -0400

    removed '

--- app/components/howWeMet.tsx
+++ app/components/howWeMet.tsx
@@ -2,7 +2,7 @@ export default function HowWeMet () {
     return (
         <main className="w-3/4 mx-auto mt-4">
             <h1 className="text-xl mb-3">How We Met</h1>
-            <p>The short story is we met on tinder... The long is a little more fun. Kenna was moving into college in Philidelphia and had stopped in North Carolina where her dad was staying for work. I talked to her and asked her on a date. She couldn't because she was leaving the day I asked. We then proceeded to talk all summer and finally went on our first date in August despite her parent's misgivings :)</p>
+            <p>The short story is we met on tinder... The long is a little more fun. Kenna was moving into college in Philidelphia and had stopped in North Carolina where her dad was staying for work. I talked to her and asked her on a date. She couldnt because she was leaving the day I asked. We then proceeded to talk all summer and finally went on our first date in August despite her parents misgivings :)</p>
         </main>
     )
 }
