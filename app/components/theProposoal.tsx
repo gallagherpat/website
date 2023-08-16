4fc2a03d1e805bdb879afb60e2825c5acf67c07d
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-15 22:34:33 -0400

    Added how we met and proposal

---
+++ app/components/theProposoal.tsx
@@ -0,0 +1,8 @@
+export default function TheProposal () {
+    return (
+        <main className="w-3/4 mx-auto mt-4">
+            <h1 className="text-xl mb-3">The Proposal</h1>
+            <p>This is another fun story. It was a pretty typical day, Kenna and I were both at work and we were texting about how what we were going to have for dinner. Then her coworker asked if we could watch her dog for the evening. That's when Kenna suggested we go on a picnic! That's when I sprung into action and got all the picninc gear and the ring, and we got engaged in the park just her and I... and Rudy the dog.</p>
+        </main>
+    )
+}
