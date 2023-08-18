d5405496b0d552de6744d9e58d0e2031a81a1e82
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-17 22:01:39 -0400

    dont break added gallery

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+"use client"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,7 +15,16 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+})
+{
+  function currentPage(href: string) {
+    let path = usePathname();
+    if(href == path){
+      return "underline underline-offset-8"
+    }else{
+      return "hover:underline underline-offset-8"
+    }
+  }
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -21,15 +32,15 @@ export default function RootLayout({
         <nav className="sticky top-0 w-full mx-auto z-50 bg-inherit pb-4">
           <br />
           <div className="flex justify-evenly w-3/4 md:w-1/2 mx-auto">
-            <a className="hover:underline underline-offset-8" href="/">Home</a>
+            <a className={currentPage("/")} href="/">Home</a>
             |
-            <a className="hover:underline underline-offset-8" href="/party">Wedding Party</a>
+            <a className={currentPage("/party")} href="/party">Wedding Party</a>
             |
-            <a className="hover:underline underline-offset-8" href="/schedule">Schedule</a>
+            <a className={currentPage("/schedule")} href="/schedule">Schedule</a>
             |
-            <a className="hover:underline underline-offset-8" href="/">Gallery</a>
+            <a className={currentPage("/gallery")} href="/gallery">Gallery</a>
             |
-            <a className="hover:underline underline-offset-8" href="/">RSVP</a>
+            <a className={currentPage("/rsvp")} href="/rsvp">RSVP</a>
           </div>
           </nav>
         {children}
