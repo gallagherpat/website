671cb339652c83beb2f41733e9dae852b61bc819
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-17 22:09:10 -0400

    Put layout to original config

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 })
 {
-  function currentPage(href: string) {
-    let path = usePathname();
-    if(href == path){
-      return "underline underline-offset-8"
-    }else{
-      return "hover:underline underline-offset-8"
-    }
-  }
+  // function currentPage(href: string) {
+  //   let path = usePathname();
+  //   if(href == path){
+  //     return "underline underline-offset-8"
+  //   }else{
+  //     return "hover:underline underline-offset-8"
+  //   }
+  // }
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -32,15 +32,15 @@ export default function RootLayout({
         <nav className="sticky top-0 w-full mx-auto z-50 bg-inherit pb-4">
           <br />
           <div className="flex justify-evenly w-3/4 md:w-1/2 mx-auto">
-            <a className={currentPage("/")} href="/">Home</a>
+            <a className="hover:underline underline-offset-8" href="/">Home</a>
             |
-            <a className={currentPage("/party")} href="/party">Wedding Party</a>
+            <a className="hover:underline underline-offset-8" href="/party">Wedding Party</a>
             |
-            <a className={currentPage("/schedule")} href="/schedule">Schedule</a>
+            <a className="hover:underline underline-offset-8" href="/schedule">Schedule</a>
             |
-            <a className={currentPage("/gallery")} href="/gallery">Gallery</a>
+            <a className="hover:underline underline-offset-8" href="/gallery">Gallery</a>
             |
-            <a className={currentPage("/rsvp")} href="/rsvp">RSVP</a>
+            <a className="hover:underline underline-offset-8" href="/rsvp">RSVP</a>
           </div>
           </nav>
         {children}
