00ed3727d2f1669ce7a4580b49ec207f63e3d462
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-17 22:10:49 -0400

    layout removed use client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-"use client"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ["latin"] })
 
