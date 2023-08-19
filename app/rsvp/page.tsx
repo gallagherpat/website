ed3a6378833552fd7bbd68c263fd84df1c59aab8
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-19 01:20:53 -0400

    Added modal to rsvp

--- app/rsvp/page.tsx
+++ app/rsvp/page.tsx
@@ -1,10 +1,20 @@
+"use client"
 import ScheduleCard from "../components/scheduleCard"
+import RSVP from "../components/rsvp"
+import React from "react";
+import { useState } from "react";
+
 export default function Page() {
+    const [modal, setModal] = useState(true);
+
     return(
         <main className="w-3/4 mx-auto mt-12 max-w-md">
-            <ScheduleCard/>
-            <ScheduleCard/>
-            <ScheduleCard lastCard={true}/>
+            <div style={{display: modal ? "none": "block"}} className="fixed inset-0 bg-opacity-70 bg-black z-50 w-screen h-screen justify-center">
+                <RSVP state={setModal}/>
+            </div>
+            <ScheduleCard id={0} state={setModal}/>
+            <ScheduleCard id={1} state={setModal}/>
+            <ScheduleCard id={2} lastCard={true} state={setModal}/>
         </main>
     )
 }
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -2,9 +2,9 @@ import ScheduleCard from "../components/scheduleCard"
 export default function Page() {
     return(
         <main className="w-3/4 mx-auto mt-12 max-w-md">
-            <ScheduleCard/>
-            <ScheduleCard/>
-            <ScheduleCard lastCard={true}/>
+            <ScheduleCard id={0}/>
+            <ScheduleCard id={1}/>
+            <ScheduleCard id={2} lastCard={true}/>
         </main>
     )
 }
