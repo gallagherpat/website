878e9b8fc55725cb1419b03173303b825342d18c
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-08-27 21:08:15 -0400

    turned imageTile into a server component

--- app/components/imageTile.tsx
+++ app/components/imageTile.tsx
@@ -1,5 +1,4 @@
 //@ts-nocheck
-"use client"
 import Image from "next/image";
 
 export default function ImageTile(props) {
@@ -18,47 +17,9 @@ export default function ImageTile(props) {
         className = "row-span-1"
     }
 
-    // if(position == 1 || position == 4 || position == 7){
-    //     image = "transition duration-150 ease-in-out rounded-lg object-cover h-full hover:scale-125 origin-top-left hover:z-10"
-    // }else if(position == 2 || position == 5 || position == 8){
-    //     image = "transition duration-150 ease-in-out rounded-lg object-cover h-full hover:scale-125 origin-center hover:z-10"
-    // }else if(position == 3 || position == 6 || position == 9){
-    //     image = "transition duration-150 ease-in-out rounded-lg object-cover h-full hover:scale-125 origin-bottom-right hover:z-10"
-    // }
-
     if(position > 9){
         return
     }
-
-    // switch(position){
-    //     case 1:
-    //     image += ' origin-top-left'
-    //     break;
-    //     case 2:
-    //     image += ' origin-top'
-    //     break;
-    //     case 3:
-    //     image += ' origin-top-right'
-    //     break;
-    //     case 4:
-    //     image += ' origin-left'
-    //     break;
-    //     case 5:
-    //     image += ' origin-center'
-    //     break;
-    //     case 6:
-    //     image += ' origin-right'
-    //     break;
-    //     case 7:
-    //     image += ' origin-bottom-left'
-    //     break;
-    //     case 8:
-    //     image += ' origin-bottom'
-    //     break;
-    //     case 9:
-    //     image += ' origin-bottom-right'
-    //     break;
-    // }
     if(env == "production"){
         return(
             <button onClick={() => {setModal(true); setImage(props.url)}} className={className}>
@@ -68,6 +29,7 @@ export default function ImageTile(props) {
                 alt="Image 1"
                 width={300}
                 height={300}
+                unoptimized
                 />
             </button>
         )
@@ -80,7 +42,6 @@ export default function ImageTile(props) {
                 alt="Image 1"
                 width={300}
                 height={300}
-                unoptimized
                 />
             </button>
         )
