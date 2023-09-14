ec86d955c1c1d963efec17b29b5fb2f7bf2b44c2
gallagherpat <48256598+gallagherpat@users.noreply.github.com>
2023-09-13 22:28:47 -0400

    Bruh

--- app/components/imageTile.tsx
+++ app/components/imageTile.tsx
@@ -13,9 +13,9 @@ export default function ImageTile(props) {
     let image = "transition duration-150 ease-in-out rounded-lg object-cover h-full hover:scale-125 hover:z-10 origin-center";
 
     if(aspectRatio < 1){
-        className = "h-auto max-w-full"
+        className = "h-auto max-w-full row-span-2"
     }else if(aspectRatio > 1){
-        className = "h-auto max-w-full"
+        className = "h-auto max-w-full row-span-1"
     }
 
     if(position > 8){
